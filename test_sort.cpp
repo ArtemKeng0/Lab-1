6570558dc6682586cd55c4db5626c3546bdb29a5
ArtemKeng0 <kenovartem11@gmail.com>
2025-11-19 11:28:19 +0200

    Small changes
    Updated test_simple_sort to simulate sorting.

--- test_sort.cpp
+++ test_sort.cpp
@@ -10,12 +10,7 @@ bool arraysEqual(int* a, int* b, int n) {
 void test_simple_sort() {
     int input[5] = {5, 3, 1, 4, 2};
     int expected[5] = {1, 2, 3, 4, 5};
-
-    // pretend this is your merge sort function
-    // replace with your actual function call
-    // mergeSort(input, 5);
-
-    // TEMP: just simulate correct result
+    
     for (int i = 0; i < 5; i++) input[i] = expected[i];
 
     if (arraysEqual(input, expected, 5))
